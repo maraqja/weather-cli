7091ad48386af18377537fd01aa163983f2a5109
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2023-06-13 15:35:02 +0300

    work with weather api better
    используем axios

--- services/api.service.js
+++ services/api.service.js
@@ -1,4 +1,5 @@
 import https from 'https'
+import axios from 'axios'
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js'
 
 const getWeather = async (city) => {
@@ -7,23 +8,32 @@ const getWeather = async (city) => {
 		throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]')
 	}
 	// const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-	const url = new URL('https://api.openweathermap.org/data/2.5/weather')
-	url.searchParams.append('q', city)
-	url.searchParams.append('appid', token)
-	url.searchParams.append('lang', 'ru')
-	url.searchParams.append('units', 'metrics')
-	https.get(url, (response) => {
-		let res = '';
-		response.on('data', (chunk) => {
-			res += chunk;
-		})
-		response.on('end', () => {
-			console.log(res)
-		})
-		response.on('error', (error) => {
+	// const url = new URL('https://api.openweathermap.org/data/2.5/weather')
+	// url.searchParams.append('q', city)
+	// url.searchParams.append('appid', token)
+	// url.searchParams.append('lang', 'ru')
+	// url.searchParams.append('units', 'metrics')
+	// https.get(url, (response) => {
+	// 	let res = '';
+	// 	response.on('data', (chunk) => {
+	// 		res += chunk;
+	// 	})
+	// 	response.on('end', () => {
+	// 		console.log(res)
+	// 	})
+	// 	response.on('error', (error) => {
 
-		})
+	// 	})
+	// })
+	const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+		params: {
+			q: city,
+			appid: token,
+			lang: 'ru',
+			units: 'metric'
+		}
 	})
+	return data
 }
 
 
